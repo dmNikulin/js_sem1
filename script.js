5122c660473d6fdba4349b8fe12b56a4de5c6ef7
Dmitrii Nikulin <138107026+dmNikulin@users.noreply.github.com>
2024-07-27 22:14:07 +0300

    added

---
+++ script.js
@@ -0,0 +1,13 @@
+let celsius = 28;
+
+let fahrenheitConversion = 1.8 * celsius + 32;
+
+console.log('Температура в градусах Цельсия: ' + celsius + '\n ' + 'Температура в градусах Фаренгейта: ' + fahrenheitConversion);
+console.log("-------------------------");
+console.log();
+
+let name = 'Дмитрий';
+let admin = '';
+admin = name;
+
+console.log(admin);
